e80667e6969e7f71a7ed3f963945a5708e91f02c
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2025-07-04 13:12:31 +0100

    Built the landing page and the login page

--- src/app/integration-hub.tsx
+++ src/app/integration-hub.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const IntegrationHub = () => {
 	return (
-		<div className="w-full flex flex-col gap-8 mt-10 items-center text-center">
+		<div className="w-full flex flex-col gap-8 mt-10 items-center text-center px-6 md:px-8">
 			<div className={"w-full flex items-center justify-start"}>
 				<Label className={"text-start"}>10+ Integrations</Label>
 			</div>
